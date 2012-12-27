a900de1b738aed413ab7d53e7d53561e4ce83f71
Leandro Marty <leanmarty@gmail.com>
2012-12-27 09:32:29 -0300

    Pruebo el GIT desde Spiral.

--- system/conf/config.conf.php
+++ system/conf/config.conf.php
@@ -4,8 +4,8 @@
 	define('DB_PASS', '');
 	define('DB_NAME', 'adm_tennis');
 	
-	define('LOG_DIR' , 'C:\xampp\htdocs\adm_lanus\log\\');
-	define('APP_PATH' , 'C:\xampp\htdocs\adm_lanus\\');
+	define('LOG_DIR' , 'C:\xampp\htdocs\admtenis\log\\');
+	define('APP_PATH' , 'C:\xampp\htdocs\admtenis\\');
 	//define('LOG_DIR' , 'D:\htdocs\lanus\log\\');
 	//define('APP_PATH' , 'D:\htdocs\lanus\\');
 	//define('APP_PATH' , 'D:\htdocs\adm_lanus\\');
